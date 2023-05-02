9e40d660403bdbb0b0da3110f7663703ad0cff9a
Willi L Stepp <willi.stepp@epfl.ch>
2023-05-02 15:31:46 +0200

    resizable, zoomable and draggable

--- eda_plugin/utility/eda_gui.py
+++ eda_plugin/utility/eda_gui.py
@@ -2,17 +2,18 @@
 
 from typing import Tuple
 from PyQt5 import QtWidgets, QtCore, QtGui
-import PyQt5
 import pyqtgraph as pg
 import numpy as np
 from pyqtgraph.graphicsItems.PlotCurveItem import PlotCurveItem
-from qimage2ndarray import gray2qimage
+from qimage2ndarray import array2qimage, gray2qimage
+import time
 
-from pymm_eventserver.data_structures import ParameterSet
+from pymm_eventserver.data_structures import ParameterSet, PyImage
 from .event_bus import EventBus
 from .qt_classes import QMainWindowRestore, QWidgetRestore
 import qdarkstyle
 
+# import matplotlib.pyplot as plt
 
 import logging
 
@@ -31,12 +32,8 @@ class EDAMainGUI(QMainWindowRestore):
         self.setWindowTitle("Event Driven Acquisition")
         self.plot = EDAPlot()
         self.central_widget = QtWidgets.QWidget()
-        self.central_widget.setLayout(QtWidgets.QVBoxLayout())
+        self.central_widget.setLayout(QtWidgets.QHBoxLayout())
 
-        if viewer:
-            self.viewer = NetworkImageViewer()
-            self.central_widget.layout().addWidget(self.viewer)
-            event_bus.new_network_image.connect(self.viewer.add_network_image)
 
         self.central_widget.layout().addWidget(self.plot)
         self.setCentralWidget(self.central_widget)
@@ -45,6 +42,11 @@ class EDAMainGUI(QMainWindowRestore):
 
         self.setStyleSheet(qdarkstyle.load_stylesheet(qt_api="pyqt5"))
 
+        if viewer:
+            self.viewer = NetworkImageViewer()
+            self.add_dock_widget(self.viewer, "Viewer", 2)
+            event_bus.new_network_image.connect(self.viewer.add_network_image)
+            event_bus.new_image_event.connect(self.viewer.add_image)
         # Make docking to this window possible
         # self.dockers = QtWidgets.QDockWidget("Dockable", self)
         # self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dockers)
@@ -54,12 +56,13 @@ class EDAMainGUI(QMainWindowRestore):
         event_bus.new_decision_parameter.connect(self.plot.add_datapoint)
         event_bus.new_parameters.connect(self.plot._set_thr_lines)
 
-    def add_dock_widget(self, widget: QWidgetRestore, name=None):
+    def add_dock_widget(self, widget: QWidgetRestore, name = None, area: int = 1):
         dock_widget = QtWidgets.QDockWidget(name, self)
         dock_widget.setWidget(widget)
         self.dock_widgets.append(dock_widget)
         self.widgets.append(widget)
-        self.addDockWidget(QtCore.Qt.DockWidgetArea(1), dock_widget)
+        self.addDockWidget(QtCore.Qt.DockWidgetArea(area), dock_widget)
+
 
     def closeEvent(self, e):
         for widget in self.widgets:
@@ -125,22 +128,88 @@ class NetworkImageViewer(QtWidgets.QGraphicsView):
         super(NetworkImageViewer, self).__init__(QtWidgets.QGraphicsScene())
         self.pixmap = QtGui.QPixmap(512, 512)
         self.setSceneRect(0, 0, 512, 512)
-        self.image = self.scene().addPixmap(self.pixmap)
+        image = np.random.randint(0, 255, (512, 512, 3), dtype=np.uint8)
+        # self.image = self.scene().addPixmap(self.pixmap)
+        image = array2qimage(image)
+        self.image = self.scene().addPixmap(QtGui.QPixmap.fromImage(image))
+        self.original_image = None
+        self.network_image = None
+        self.stacked = np.zeros((512, 512, 3), dtype=np.float16)
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        self.red_lut = [QtGui.qRgb(i, 0, 0) for i in range(256)]
+        self.gray_lut = [QtGui.qRgb(i, i, i) for i in range(256)]
+
 
     def _reset_scene_rect(self, shape: Tuple):
         self.setSceneRect(0, 0, *shape)
+        self.pixmap = QtGui.QPixmap(*shape)
         self.fitInView(0, 0, *shape, mode=QtCore.Qt.KeepAspectRatio)
         self.setBackgroundBrush(QtGui.QColor("#222222"))
 
     @QtCore.pyqtSlot(np.ndarray, tuple)
     def add_network_image(self, image: np.ndarray, dims: tuple):
         """Translate the input image into a QImage and display in the scene."""
-        log.debug(f"New image in ImageViewer with shape {image.shape}")
-        if dims[0] == 0:
-            self._reset_scene_rect(image.shape)
-        image = gray2qimage(image, normalize=True)
-        self.image.setPixmap(QtGui.QPixmap.fromImage(image))
-        self.update()
+        t0 = time.perf_counter()
+        self.network_image = image
+        # qimage = self.screen_images()
+        # self.image.setPixmap(QtGui.QPixmap.fromImage(qimage))
+
+
+        self.pixmap.fill(QtGui.QColor("transparent"))
+
+        self.painter = QtGui.QPainter()
+
+        # here the two images are drawn into the pixmap
+        self.painter.begin(self.pixmap)
+        self.painter.setCompositionMode(QtGui.QPainter.CompositionMode.CompositionMode_Screen)
+
+        norm_image = self.original_image / (np.max(self.original_image)/255)
+        norm_net_image = self.network_image / (np.max(self.network_image)/255)
+
+        # choose composition mode
+        qimage_1 = gray2qimage(norm_image.astype(np.uint8))
+        qimage_2 = gray2qimage(norm_net_image.astype(np.uint8))
+        qimage_1.setColorTable(self.gray_lut)
+        qimage_2.setColorTable(self.red_lut)
+        self.painter.drawImage(0,0, qimage_1)
+        self.painter.drawImage(0,0, qimage_2)
+        self.painter.end()
+
+        self.image.setPixmap(self.pixmap)
+        log.info(f"Screening took {time.perf_counter() - t0} seconds =======")
+        log.info(f"{norm_net_image.max()}")
+
+    @QtCore.pyqtSlot(PyImage)
+    def add_image(self, image: PyImage):
+        """Translate the input image into a QImage and display in the scene."""
+        self.original_image = image.raw_image
+        if image.timepoint == 0:
+            self.stacked = np.zeros(list(self.original_image.shape) + [3], dtype=np.float16)
+            self._reset_scene_rect(self.original_image.shape)
+
+    def screen_images(self):
+        """Take both images and combine them into one false color image."""
+        norm_image = self.original_image - self.original_image.min()
+        norm_net_image = self.network_image - self.network_image.min()
+        norm_image = norm_image / np.max(norm_image)
+        norm_net_image = norm_net_image / np.max(norm_net_image)
+
+        self.stacked[..., 0] = norm_net_image*10 # set the red channel to the second grayscale image
+        self.stacked[..., 1] = norm_image # set the green channel to zero
+        false_color_image = (self.stacked * 255.0).astype(np.uint8)
+        return array2qimage(false_color_image)
+
+    def wheelEvent(self, event):
+        if event.modifiers() & QtCore.Qt.ControlModifier:
+            # zoom
+            anchor = self.transformationAnchor()
+            self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+            angle = event.angleDelta().y()
+            factor = 1.1 if angle > 0 else 0.9
+            self.scale(factor, factor)
+            self.setTransformationAnchor(anchor)
+        else:
+            QtWidgets.QGraphicsView.wheelEvent(self, event)
 
 
 class NapariImageViewer(QtWidgets.QWidget):
